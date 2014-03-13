0a488e0dce38f27aa88abcc1c87c4882e78e605b
Chapman Shoop <chapman.shoop@gmail.com>
2014-03-13 15:36:36 -0700

    Renaming test files so Django 1.6 will find them

--- categories/tests/__init__.py
+++ categories/tests/__init__.py
@@ -1,6 +1,10 @@
-from categories.tests.category_import import *
-from categories.tests.templatetags import *
-from categories.tests.manager import *
-from categories.tests.registration import *
-
-__fixtures__ = ['categories.json']
+# Test discovery in Django 1.4 and 1.5 looks for test cases in the `tests`
+# module. So we include all of our tests here for those versions to discover.
+#
+# In Django 1.6, files matching `test*.py` are discovered and run. Maybe we
+# won't need these in the future.
+# 
+from categories.tests.test_category_import import *
+from categories.tests.test_templatetags import *
+from categories.tests.test_manager import *
+from categories.tests.test_registration import *
