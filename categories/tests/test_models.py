2d99b5554f0665fd973151ff0ec8aa03ec041c3a
Chapman Shoop <chapman.shoop@gmail.com>
2014-03-13 23:10:36 -0700

    Test function names match content of tests

--- categories/tests/test_models.py
+++ categories/tests/test_models.py
@@ -6,12 +6,12 @@ class CategoryTestCase(TestCase):
 
     # Category.objects.active() only returns categories that are active.
     #
-    def test_objects_active_inlcudes_active_category(self):
+    def test_objects_active_excludes_inactive_category(self):
         category = Category.create(active=False)
         with self.assertRaises(Category.DoesNotExist):
             Category.objects.active().get(pk=category.pk)
 
-    def test_objects_active_excludes_inactive_category(self):
+    def test_objects_active_includes_active_category(self):
         category = Category.create(active=True)
         self.assertEqual(
             category,
