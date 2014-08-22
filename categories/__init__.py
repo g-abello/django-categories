7d09f0d486a8a8fbd83b37d9c31d5d4b0a4d1af8
Chapman Shoop <chapman.shoop@gmail.com>
2014-08-22 10:15:12 -0700

    Bumping version to next realease target (2.0.0)

--- categories/__init__.py
+++ categories/__init__.py
@@ -1,9 +1,9 @@
 __version_info__ = {
-    'major': 1,
-    'minor': 2,
-    'micro': 2,
-    'releaselevel': 'final',
-    'serial': 1
+    'major': 2,
+    'minor': 0,
+    'micro': 0,
+    'releaselevel': 'beta',
+    'serial': 0
 }
 
 
