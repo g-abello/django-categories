76cbb57d31bd206d9aa258cc58d4da4bb5b336a3
Chapman Shoop <chapman.shoop@gmail.com>
2014-08-22 09:49:52 -0700

    Updating author details in setup.py

--- setup.py
+++ setup.py
@@ -17,10 +17,10 @@ setup(
     version=categories.get_version(),
     description='A way to handle one or more hierarchical category trees in django.',
     long_description=long_description,
-    author='Corey Oordt',
-    author_email='coreyoordt@gmail.com',
+    author='Chapman Shoop',
+    author_email='chapman.shoop@gmail.com',
     include_package_data=True,
-    url='http://github.com/callowayproject/django-categories',
+    url='http://github.com/rbgb/django-categories',
     packages=find_packages(),
     classifiers=[
         'Framework :: Django',
