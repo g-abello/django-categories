616c30ce3b15c9110b8d07fa97eecc8a3f48bd70
Chapman Shoop <chapman.shoop@gmail.com>
2014-08-24 21:45:26 -0700

    Dropping Django 1.3- related code

--- categories/settings.py
+++ categories/settings.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import (
+    absolute_import,
+    division,
+    print_function,
+    unicode_literals,
+)
+
 from django.conf import settings
 from django.db.models import Q
 from django.utils.translation import ugettext_lazy as _
